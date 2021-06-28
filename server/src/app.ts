4afa366815d64f4e6f7b94b85da75050597a44be
David Han <davidhannn@yahoo.com>
2021-06-28 16:01:41 -0700

    updated config for auth service

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,12 +20,12 @@ import bugRoutes from './routes/bugs';
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.set('trust proxy', 1);
 app.use(trim);
 // app.use(cookieParser());
-// app.use(cors());
 // app.use(
 //   cors({
 //     credentials: true,
@@ -38,27 +38,27 @@ app.use(trim);
 //   app.use(express.static(path.join(__dirname, 'client/build')));
 // }
 
-var whitelist = [
-  'https://bug-tracker-pern.netlify.app',
-  'http://bug-tracker-pern.netlify.app',
-];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
-};
+// var whitelist = [
+//   'https://bug-tracker-pern.netlify.app',
+//   'http://bug-tracker-pern.netlify.app',
+// ];
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false }; // disable CORS for this request
+//   }
+//   callback(null, corsOptions); // callback expects two parameters: error and options
+// };
 
 // var corsOptions = {
 //   origin: 'https://bug-tracker-pern.netlify.app/',
 //   // origin: ['http://localhost:3000', 'https://bug-tracker-pern.netlify.app'],
 //   credentials: true,
 //   optionsSuccessStatus: 200,
-// };
-app.use(cors(corsOptionsDelegate));
+// // };
+// app.use(cors(corsOptionsDelegate));
 // app.options('*', cors())
 
 app.use(express.static('public'));
