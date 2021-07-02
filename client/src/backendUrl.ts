b84200bce562bc4fa97ac5265d604f77741059b2
David Han <davidhannn@yahoo.com>
2021-07-01 18:00:11 -0700

    added navbar routing, added notifications for authentication

--- client/src/backendUrl.ts
+++ client/src/backendUrl.ts
@@ -1,5 +1,5 @@
-// const backendUrl = 'https://bugtrackerfinal.herokuapp.com';
+const backendUrl = 'https://bugtrackerfinal.herokuapp.com';
 
-const backendUrl = 'http://localhost:5000';
+// const backendUrl = 'http://localhost:5000';
 
 export default backendUrl;
